f13b33594434b3b8a9ef4f06cffd3529dbc71f0e
Arya Shantanu <aryas.ug23.ec@nitp.ac.in>
2026-02-04 14:58:15 +0530

    Create page.jsx

---
+++ src/app/embed/page.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>My Website</h1>
+
+      <div id="docvya-docs"></div>
+
+      <script
+        src="http://localhost:3000/embed.js"
+        data-docvya-embed-key="gAAAAABpfyttU9sJ5v4MpeoE0piLxzjXaMxwDNm4O57CicbdYpwoyFpjfqgdZ9r-4x02O5rH5Cg5i6GNn5UbTntUo_-FF2ifBV504wvAzmuApQhBfZLfjzMKWxzSzA4yvmLxR-RXhdHg"
+      ></script>
+    </div>
+  )
+}
